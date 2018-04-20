4e09843cc17b1ca4cd4e6856308040fcaf8b119f
gitFoxCode <Sebeklis132@gmail.com>
2018-04-20 15:22:36 +0200

    hotfix

--- foxbot.js
+++ foxbot.js
@@ -459,11 +459,21 @@ var commands = [ // All commands starts here:
         desc: "Kalkulator",
         func: (api, event, args) => {
             /// VALIDATE THIS [ TO FIX ] [[D A N G E R]]
-            if (args == "process.exit()"){
-                api.sendMessage(`UWAGA! \n BOT ZOSTAŁ ZHACKOWANY!!!!! `+ mathResult, event.threadID);
-            }
-             let mathResult = foxMath.eval(args);
-             api.sendMessage(`Wynik: `+ mathResult, event.threadID);
+            try {
+                if (args == "process.exit()"){
+                    api.sendMessage(`UWAGA! \n BOT ZOSTAŁ ZHACKOWANY!!!!! `+ mathResult, event.threadID);
+                }
+                 let mathResult = foxMath.eval(args);
+                 api.sendMessage(`Wynik: `+ mathResult, event.threadID);
+
+             } catch (e) {
+                 if (e instanceof SyntaxError) {
+                     api.sendMessage(`*Błąd:* `+ e, event.threadID);
+                 }
+             }
+
+
+
 
 
         }
